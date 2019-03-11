b22e013729ad0ca4c778d49af4a714639d19771d
Malubian906 <xudong_zhang1988@outlook.com>
2019-03-11 22:23:51 +0800

    线性表虚基类

--- AlgorithmDataStructure/MyDataStructure.h
+++ AlgorithmDataStructure/MyDataStructure.h
@@ -1,9 +1,19 @@
 #pragma once
-class MyClass
+#include <iosfwd>
+#include <ostream>
+#include <istream>
+#include <iostream>
+
+//  线性表功能几类
+template<typename T>
+class LinearList
 {
-public:
-	MyClass() {};
-	~MyClass() {};
-	void func_1() {};
-	void func_2() {};
+	virtual ~LinearList() {};
+	virtual  bool empty() const = 0;
+	virtual  int size() const = 0;
+	virtual T& get(int theIndex) const = 0;
+	virtual int indexOf(const T& theElement) const = 0;
+	virtual void erase(int theIndex) = 0;
+	virtual void insert(int theIndex, const T& theElement) = 0;
+	virtual void output(std::ostream& out) const = 0;
 };
--- AlgorithmDataStructure/main.cpp
+++ AlgorithmDataStructure/main.cpp
@@ -2,17 +2,7 @@
 #include "MyDataStructure.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])                
 {
-
-	MyClass *p = nullptr;
-	p->func_1();
-	p->func_2();
-
-
-	QApplication a(argc, argv);
-	AlgorithmDataStructure w;
-	w.show();
-	//
-	return a.exec();
+	return 0;
 }
