0d95386dabca28dbd6550bbda585e06f48cb6803
ZhangXD <xudong_zhang1988@outlook.com>
2019-03-11 20:24:58 +0800

    home

---
+++ AlgorithmDataStructure/main.cpp
@@ -0,0 +1,11 @@
+#include "AlgorithmDataStructure.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	AlgorithmDataStructure w;
+	w.show();
+	//
+	return a.exec();
+}
